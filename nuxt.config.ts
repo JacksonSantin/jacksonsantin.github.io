2f97953f6e18241ed3a15e5a6c3e415fa11e0221
JacksonSantin <jackdhanyelsn@gmail.com>
2024-02-20 15:16:49 -0300

    :sparkles: Feat(portfolio): Atualiza versão das dependências.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default {
+export default defineNuxtConfig({
+  ssr: true,
   devtools: { enabled: true },
   css: ['~/assets/css/main.css', '~/assets/css/global.css'],
-  head: {
-    link: [
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap",
-      },
-    ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap",
+        },
+      ],
+    },
   },
   modules: ['@nuxtjs/i18n', 'nuxt-icon'],
   i18n: {
@@ -20,4 +23,4 @@ export default {
       autoprefixer: {},
     },
   },
-}
+})
